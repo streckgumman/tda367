161f08745dd0d6a375f12eb5e580d873bbf1f61e
Vargen18 <45463154+vargen18@users.noreply.github.com>
2019-09-19 15:05:13 +0200

    "Tested" the "setFullscreen" method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 import model.Game;
+import view.Screen;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Game game = new Game();
+        Screen screen = new Screen();
+        testScreen(screen);
+    }
+
+    /**
+     *
+     * @param screen the "screen" (not the physical screen)
+     */
+    public static void testScreen(Screen screen){
+        JFrame window = new JFrame();
+        DisplayMode displayMode = new DisplayMode(1920, 1080,32, 60);
+        screen.setFullscreen(displayMode, window);
     }
 
 }
