3f1b7a7f1b9224d3fd773c3bb22b9ef1362748dc
Amanda <amandadehlen@gmail.com>
2019-09-25 10:21:11 +0200

    Create MouseInput.java

---
+++ src/controller/MouseInput.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseInput implements MouseListener {
+
+
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+}
