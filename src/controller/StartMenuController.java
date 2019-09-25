1d8d65794976d09cc36178ccfcf8898ac92b6d62
Jononannes <johannes.kvernes@gmail.com>
2019-09-25 12:49:11 +0200

    Testing new view structure

--- src/controller/StartMenuController.java
+++ src/controller/StartMenuController.java
@@ -13,6 +13,8 @@ public class StartMenuController extends Controller{
             public void update(KeyEvent event) {
                 if(event.getKeyCode() == java.awt.event.KeyEvent.VK_E){
                     System.out.println("E-key pressed");//Nästa steg så ska vi starta InGameView
+                } else if (event.getKeyCode() == java.awt.event.KeyEvent.VK_SPACE) {
+                    redrawView();
                 }
             }
         };
