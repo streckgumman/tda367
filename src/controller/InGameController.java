c8bd079e90b718d943398bf18fe12f1ac721e775
Linnea Johansson <43148111+streckgumman@users.noreply.github.com>
2019-09-26 16:04:40 +0200

    Implemented methods from abstract class

--- src/controller/InGameController.java
+++ src/controller/InGameController.java
@@ -9,10 +9,13 @@ import java.awt.event.KeyEvent;
 public class InGameController extends Controller{
 
     public InGameController(View view, Game game) {
-        super(view, null, game);
+        super(view, game);
         addUpdater(new PlayerUpdater(game.getPlayer()));
     }
 
+    protected void updateModel() {
+
+    }
 
 
 }
