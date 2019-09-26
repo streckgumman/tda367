6583329a9ec4852bd5b66bd3454f80b27cf95e0e
Linnea Johansson <43148111+streckgumman@users.noreply.github.com>
2019-09-26 16:13:02 +0200

    Player can move up, down, left, right
    Very slowly

--- src/model/Player.java
+++ src/model/Player.java
@@ -11,8 +11,19 @@ public class Player extends Charakter {
 
     public void move(String string) {
         if(string.equals("left")){
-            this.setXPosition((int)this.getPosition().getX()-300);
-            System.out.println("Moves");
+            this.setXPosition((int)this.getPosition().getX()-10);
+            System.out.println(this.getPosition().toString());
+        }
+        if(string.equals("right")){
+            this.setXPosition((int)this.getPosition().getX()+10);
+            System.out.println(this.getPosition().toString());
+        }
+        if(string.equals("up")){
+            this.setYPosition((int)this.getPosition().getY()-10);
+            System.out.println(this.getPosition().toString());
+        }
+        if(string.equals("down")){
+            this.setYPosition((int)this.getPosition().getY()+10);
             System.out.println(this.getPosition().toString());
         }
     }
