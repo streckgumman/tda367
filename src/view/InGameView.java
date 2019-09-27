33c583b013a3e0fa5850f8c4bae934caeb346763
Vargen18 <45463154+vargen18@users.noreply.github.com>
2019-09-27 10:55:10 +0200

    Drawn player model now depends on player orientation
    horizontalFlip and copyImage are used to create the flipped image

--- src/view/InGameView.java
+++ src/view/InGameView.java
@@ -1,9 +1,12 @@
 package view;
 
 import model.Game;
+import model.Player;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +15,7 @@ public class InGameView extends View {
 
     private BufferedImage background;
     private BufferedImage character;
+    private BufferedImage flippedCharacter;
     private Point characterPos;
 
     public InGameView(Game game) {
@@ -26,12 +30,35 @@ public class InGameView extends View {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+        flippedCharacter = horizontalFlip(copyImage(character));
     }
 
     void draw(Graphics g) {
         g.drawImage(background, 0, 0, 1920, 1080, null);
         characterPos = game.getPlayer().getPosition();
-        g.drawImage(character, (int) characterPos.getX(), (int) characterPos.getY(), character.getWidth() / 4, character.getHeight() / 4, null);
+        if (game.getPlayer().getPlayerOrientation() == Player.Orientation.RIGHT) {
+            g.drawImage(character, (int) characterPos.getX(), (int) characterPos.getY(), character.getWidth() / 4, character.getHeight() / 4, null);
+        } else {
+            g.drawImage(flippedCharacter, (int) characterPos.getX(), (int) characterPos.getY(), flippedCharacter.getWidth() / 4, flippedCharacter.getHeight() / 4, null);
+        }
+    }
 
+    //TODO These (copyImage and horizontalFlip) methods might not belong in this class?
+    private BufferedImage copyImage(BufferedImage source) {
+        BufferedImage b = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+        Graphics g = b.getGraphics();
+        g.drawImage(source, 0, 0, null);
+        g.dispose();
+        return b;
+    }
+
+    private BufferedImage horizontalFlip(BufferedImage image) {
+        AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+        tx.translate(-image.getWidth(null), 0);
+
+        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        image = op.filter(image, null);
+        return image;
     }
 }
