50113e7cdf42f3378abbb6da514f153027d281ed
Vargen18 <45463154+vargen18@users.noreply.github.com>
2019-09-27 10:53:25 +0200

    Formatting

--- src/model/Player.java
+++ src/model/Player.java
@@ -13,26 +13,26 @@ public class Player extends Charakter {
     private Orientation playerOrientation = Orientation.RIGHT;
 
     public Player(String name) {
-        super(name, new Point(1920/2,1080/2));
+        super(name, new Point(1920 / 2, 1080 / 2));
     }
 
     public void move(String string) {
-        if(string.equals("left")){
-            this.setXPosition((int)this.getPosition().getX()-10);
+        if (string.equals("left")) {
+            this.setXPosition((int) this.getPosition().getX() - 10);
             System.out.println(this.getPosition().toString());
             playerOrientation = Orientation.LEFT;
         }
-        if(string.equals("right")){
-            this.setXPosition((int)this.getPosition().getX()+10);
+        if (string.equals("right")) {
+            this.setXPosition((int) this.getPosition().getX() + 10);
             System.out.println(this.getPosition().toString());
             playerOrientation = Orientation.RIGHT;
         }
-        if(string.equals("up")){
-            this.setYPosition((int)this.getPosition().getY()-10);
+        if (string.equals("up")) {
+            this.setYPosition((int) this.getPosition().getY() - 10);
             System.out.println(this.getPosition().toString());
         }
-        if(string.equals("down")){
-            this.setYPosition((int)this.getPosition().getY()+10);
+        if (string.equals("down")) {
+            this.setYPosition((int) this.getPosition().getY() + 10);
             System.out.println(this.getPosition().toString());
         }
     }
