16891f416ec040960b7bdaf7021be0eba83de3e9
nilsanna <nilsson.j.anna@gmail.com>
2019-09-30 11:25:51 +0200

    Added new instance variable and getters for them

--- src/model/Level.java
+++ src/model/Level.java
@@ -2,13 +2,35 @@ package model;
 
 public abstract class Level {
 
-    Player player;
-    NPC[] npcs;
-    Puzzle[] puzzles;
-    Trap[] traps;
-    Item[] items;
+    private Player player;
+    private NPC[] npcs;
+    private Puzzle[] puzzles;
+    private Trap[] traps;
+    private Item[] items;
 
     public Level() {
 
     }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public NPC[] getPlayers() {
+        return npcs;
+    }
+
+    public Puzzle[] getPuzzles() {
+        return puzzles;
+    }
+
+    public Trap[] getTraps() {
+        return traps;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+
 }
