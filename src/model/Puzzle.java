618bee6a51cad00f4fe52310707c27a56a942659
nilsanna <nilsson.j.anna@gmail.com>
2019-09-30 11:20:56 +0200

    Groundwork for interacting with Puzzle
    - Puzzle now implements Interactable
    - Addded method something (a placeholder)
    - Added method interact
    Groundwork for interacting with

--- src/model/Puzzle.java
+++ src/model/Puzzle.java
@@ -2,9 +2,20 @@ package model;
 
 import java.awt.*;
 
-public abstract class Puzzle extends GameObject{
+public abstract class Puzzle extends GameObject implements Interactable{
     private boolean solved = false;
     public Puzzle() {
         super(new Point(0,0));
     }
+
+    //vet inte om detta (under) ska vara kvar
+    public void something(){
+        //Världen ska vända tillbaka sig rätt och dörren till nästa nivå ska öppnas
+    }
+
+    public void interact(GameObject go){
+        something();
+    }
+
+
 }
