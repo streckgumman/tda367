0288f97592b960cab11c90feaaa7852bfbe46e10
nilsanna <nilsson.j.anna@gmail.com>
2019-09-30 11:22:35 +0200

    Groundwork for interacting with items
    - Item now implements Interactable
    - Addded method pickup
    - Added method drop
    - Added abstract method use
    - Added method interact

--- src/model/Item.java
+++ src/model/Item.java
@@ -2,10 +2,33 @@ package model;
 
 import java.awt.*;
 
-public abstract class Item extends GameObject{
+public abstract class Item extends GameObject implements Interactable{
     private String name;
     public Item(String name) {
         super(new Point(0,0));
         this.name = name;
     }
+
+    public void pickup(Player player){
+        //ska lägga till objektet i spelarens ryggsäck
+        player.pickUpItem(this);
+    }
+
+    public void drop(){
+
+        //ska släppa objektet på "marken", ge tillbaka det till level
+        //behöver ha tillgång till leveln
+    }
+
+    public abstract void use();
+
+
+
+    public void interact(GameObject go){
+        // bad code, will be changed
+        if (go instanceof Player) {
+            pickup((Player) go);
+        }
+    }
+
 }
