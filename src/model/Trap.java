a2823a8a320d77b543eb82409b95d0e74c71f039
nilsanna <nilsson.j.anna@gmail.com>
2019-09-30 11:19:55 +0200

    Groundwork for interacting with traps
    - Traps now implements Interactable
    - Addded method activate
    - Added method interact

--- src/model/Trap.java
+++ src/model/Trap.java
@@ -2,11 +2,20 @@ package model;
 
 import java.awt.*;
 
-public abstract class Trap extends GameObject{
+public abstract class Trap extends GameObject implements Interactable{
 
 
     public Trap() {
         super(new Point(0,0));
 
     }
+
+    public void activate(){
+        //fällan ska aktiveras och spelaren ska få en "debuff"
+    }
+
+    public void interact(GameObject go){
+        activate();
+    }
+
 }
