26d8c4a44f912c20d4140a099490f2c47f6a1cc9
nilsanna <nilsson.j.anna@gmail.com>
2019-10-01 13:16:39 +0200

    Gave DogWithGun position and size

--- src/model/npcs/DogWithGun.java
+++ src/model/npcs/DogWithGun.java
@@ -6,7 +6,7 @@ import model.NPCType;
 public class DogWithGun extends NPC {
 
     public DogWithGun(String name) {
-        super(name, NPCType.DOGWITHGUN);
+        super(name, NPCType.DOGWITHGUN, 0, 0, 625 / 4, 532 / 4);
     }
 
     protected void update() {
