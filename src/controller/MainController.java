189c2db798c3178af8514ff947fd3f4aa7c01a75
Vargen18 <45463154+vargen18@users.noreply.github.com>
2019-10-01 11:32:24 +0200

    MainController now runs the game instead of Main.

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Game;
 import view.InGameView;
+import view.StartMenuView;
 import view.View;
 
 import javax.swing.*;
@@ -10,19 +11,42 @@ public class MainController {
 
     private JFrame frame;
     private final Game game;
+
     private Controller c;
     private boolean running;
+
+    private int windowWidth;
+    private int windowHeight;
 
     public MainController() {
         game = new Game();
         frame = new JFrame();
+
+        windowWidth = 1920; //For now the width and height is 1920x1080, but it could easily take an argument from settings instead
+        windowHeight = 1080;
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void startGame() {
+        final View view;
+        frame.add(view = new StartMenuView());
 
+
+        frame.setSize(windowWidth, windowHeight - 100);
+        frame.setResizable(false);
+        frame.setUndecorated(true);
+        frame.pack();
+        frame.setVisible(true);
+
+        c = new StartMenuController(view);
+        view.addKeyListener(c);
+
+        runGame();
     }
 
     private void runGame() {
+        running = true;
         // Calls the sub-controller's update method 60 times per second (ideally)
         while (running) {
             long startTime = System.currentTimeMillis();
