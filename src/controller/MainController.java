f40eeed7e6915f98a38f6d26323364c8fd30cbd2
Vargen18 <45463154+vargen18@users.noreply.github.com>
2019-10-01 13:43:42 +0200

    Documentation and formatting

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -7,18 +7,26 @@ import view.View;
 
 import javax.swing.*;
 
+/**
+ * The "overlord" of controllers, responsible for switching which one is active, as well as which view is shown.
+ */
 public class MainController {
 
+    private final Game game;
     private JFrame frame;
     private View view;
-    private final Game game;
-
+    /**
+     * The currently active controller.
+     */
     private Controller c;
     private boolean running;
 
     private int windowWidth;
     private int windowHeight;
 
+    /**
+     * Constructor
+     */
     public MainController() {
         game = new Game();
         frame = new JFrame();
@@ -29,9 +37,11 @@ public class MainController {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    /**
+     * Creates a window with the content of StartMenuView and makes StartMenuController the active controller. (shows the start menu)
+     */
     public void startGame() {
         frame.add(view = new StartMenuView());
-
 
         frame.setSize(windowWidth, windowHeight - 100);
         frame.setResizable(false);
@@ -45,6 +55,9 @@ public class MainController {
         runGame();
     }
 
+    /**
+     * The core loop of the game, updating the model and view approximately 60 times per second.
+     */
     private void runGame() {
         running = true;
         // Calls the sub-controller's update method 60 times per second (ideally)
@@ -63,15 +76,18 @@ public class MainController {
         }
     }
 
+    /**
+     * Switches view to the in-game view, and makes InGameController the active controller. (Changes to the screen where you play the game)
+     */
     public void switchToIngame() {
         frame.remove(view); //I don't know why this is necessary, but I could not get it to work without first removing the view and then adding it again. /Vargen
         view = new InGameView(game);
         frame.add(view);
-        frame.validate();
+        frame.validate(); //Removing the view invalidates the frame, so we need to validate it in order to display it again.
 
         c = new InGameController(view, game);
         view.addKeyListener(c);
-        view.requestFocus();
+        view.requestFocus(); //This is required in order to register user input again.
 
     }
 
