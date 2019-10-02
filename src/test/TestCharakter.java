aa55e0b53ac2405fb4d681ee75980b7c09cf6cc3
Amanda <amandadehlen@gmail.com>
2019-10-02 10:29:59 +0200

    Update TestCharakter.java

--- src/test/TestCharakter.java
+++ src/test/TestCharakter.java
@@ -12,7 +12,7 @@ public class TestCharakter {
     public void charaktersNameCanBeAskedAfter(){
         Charakter player = new Player("Duckoo");
 
-        assertEquals(player.getName().toString(), "Duckoo");
+        assertEquals(player.getName(), "Duckoo");
     }
 
     @Test
