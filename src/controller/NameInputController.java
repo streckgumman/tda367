8ca5f0f9bd77dfa8b01991b6b2c4f4285d4c3f1a
Vargen18 <45463154+vargen18@users.noreply.github.com>
2019-10-04 15:29:16 +0200

    NameInputScreen now prompts the player to enter their name and saves it.

--- src/controller/NameInputController.java
+++ src/controller/NameInputController.java
@@ -7,13 +7,12 @@ import java.awt.event.KeyEvent;
 
 public class NameInputController extends Controller {
 
-    private View view;
     private Game game;
 
     @SuppressWarnings("unchecked")
     public NameInputController(View v, MainController mainController) {
-        super(v, null);
-        this.view = v;
+        super(v, mainController.getGame());
+        v.setFocusTraversalKeysEnabled(false); //Don't let Tab bork the entire thing.
         this.game = mainController.getGame();
 
         Updater u = new Updater(null) {
@@ -21,6 +20,8 @@ public class NameInputController extends Controller {
             public void keyPressed(KeyEvent event) {
                 if (event.getKeyCode() == KeyEvent.VK_ESCAPE) {
                     mainController.exitGame();
+                } else if (event.getKeyCode() == KeyEvent.VK_ENTER) {
+                    mainController.switchToIngame();
                 }
             }
 
@@ -31,19 +32,32 @@ public class NameInputController extends Controller {
 
             @Override
             public void keyTyped(KeyEvent event) {
-                //playerName += event.getKeyChar();
+                updatePlayerName(event.getKeyChar());
             }
         };
+
 
         addUpdater(u);
     }
 
-
+    /**
+     * Adds a character to the player's name, unless that character is '\b' (backspace), in which case it removes one character from the name.
+     *
+     * @param c The character to add to the Player's name
+     */
+    private void updatePlayerName(char c) {
+        String playerName = game.getPlayer().getName();
+        if (c == '\b') { //If the character that was input is backspace:
+            game.getPlayer().setName(playerName.substring(0, Math.max(playerName.length() - 1, 0))); //Delete the last character in player name
+        } else if ((int) c != 127) { //If the character that was input is also not delete:
+            playerName += c;
+            game.getPlayer().setName(playerName);
+        }
+    }
 
     @Override
     protected void redrawView() {
         super.redrawView();
-        //view.drawText()
     }
 
     @Override
