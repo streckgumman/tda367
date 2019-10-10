fd53f3d00758ee1682f4e63a6ba7e9e55b3b676c
Linnea Johansson <43148111+streckgumman@users.noreply.github.com>
2019-10-10 14:45:38 +0200

    Made tests for model; Game and Level

---
+++ src/test/TestGame.java
@@ -0,0 +1,41 @@
+package test;
+
+import model.Game;
+import model.Level;
+import model.Player;
+import model.levels.Level1;
+import model.levels.Level2;
+import model.levels.Level3;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * @author Linnea Johansson
+ *
+ * Class to test the Game Class
+ */
+
+public class TestGame {
+
+    Game game;
+
+
+    /**
+     * Testing if the game knows to go to next level.
+     * In Game class, Level1, Level2 and Level3 is currently initialized.
+     */
+    @Test
+    public void shouldGoToNextLevel(){
+        game = new Game();
+        Level testLevel1 = game.getLevel();
+        game.nextLevel();
+        assertFalse(testLevel1 == game.getLevel());
+        Level testLevel2 = game.getLevel();
+        game.nextLevel();
+        assertFalse(testLevel1 == testLevel2);
+        assertFalse(testLevel2 == game.getLevel());
+    }
+
+}
