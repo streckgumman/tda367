9c457b70c9518a96991f7284f006274dbb5beeb5
Amanda <amandadehlen@gmail.com>
2019-10-10 13:21:44 +0200

    Added comments

--- src/view/InLevel2View.java
+++ src/view/InLevel2View.java
@@ -4,18 +4,25 @@ import model.Game;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * @author Amanda Dehlén
+ * @author Linnea Johansson
+ *
+ * InLevel2View controlls the view that are shown when the game is in level 2
+ */
 public class InLevel2View extends InGameView {
     /**
-     * Class constructor.
-     *
+     * The public constructor that creates an instance of the class InLevel2View
      * @param game the model's game.
      */
     public InLevel2View(Game game) {
         super(game);
     }
 
-    @Override
-    protected BufferedImage setBackground(String background){
+    /**
+     * @return the image that should be set as the background in level 2
+     */
+    protected BufferedImage setBackground(String dummy){
         return super.setBackground("./resources/backgroundLevel2.jpeg");
     }
 
