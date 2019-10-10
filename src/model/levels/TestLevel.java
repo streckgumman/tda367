8e24b05c0f47fd3e825ac2fa4a44a265f468c703
Linnea Johansson <43148111+streckgumman@users.noreply.github.com>
2019-10-10 14:46:24 +0200

    Made a TestLevel to be able to test levels

---
+++ src/model/levels/TestLevel.java
@@ -0,0 +1,23 @@
+package model.levels;
+
+import model.Level;
+
+/**
+ * @author Amanda Dehlén
+ * @author Linnea Johansson
+ *
+ * TestLevel exists to test the methods of the class Level and extends Level
+ *
+ * */
+public class TestLevel extends Level {
+
+    /**
+     * The public constructor of the class TestLevel
+     *
+     * @param nrInLine which position in the order of levels the level has
+     */
+    public TestLevel(int nrInLine) {
+        super(nrInLine);
+    }
+
+}
