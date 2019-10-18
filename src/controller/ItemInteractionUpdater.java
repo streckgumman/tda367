267c1ef7ca623d8238849ffcf82b9f16beec11e8
nilsanna <nilsson.j.anna@gmail.com>
2019-10-18 15:14:37 +0200

    Added docuemntation

--- src/controller/ItemInteractionUpdater.java
+++ src/controller/ItemInteractionUpdater.java
@@ -4,15 +4,34 @@ import model.*;
 
 import java.awt.event.KeyEvent;
 
+/**
+ * @author Johannes Kvernes
+ * @author Anna Nilsson
+ *
+ * Updater for all interactions with items
+ */
 public class ItemInteractionUpdater extends Updater<Player> {
 
     private Game game;
 
+
+    /**
+     * Constructor
+     *
+     * @param player the players that interacts with items
+     * @param game the game that the items reside in
+     */
     protected ItemInteractionUpdater(Player player, Game game) {
         super(player);
         this.game = game;
     }
 
+    /**
+     * Depending on the key event calls method
+     * itemDetector or dropItem
+     *
+     * @param event
+     */
     public void keyPressed(KeyEvent event) {
         if (event.getKeyCode() == KeyEvent.VK_E) {
             itemDetector();
@@ -21,6 +40,10 @@ public class ItemInteractionUpdater extends Updater<Player> {
         }
     }
 
+    /**
+     * Method that drops the players item into the world
+     * if the player is holding one
+     */
     private void dropItem() {
         if (getGameObject().getItem() != null) {
             getGameObject().getItem().setPosition(getGameObject().getX(), getGameObject().getY());
@@ -29,6 +52,9 @@ public class ItemInteractionUpdater extends Updater<Player> {
         }
     }
 
+    /**
+     * Method that checks if an item is intersected
+     */
     private void itemDetector() {
         for (int j = game.getLevel().getItems().size() - 1; j >= 0; j--) {
             Item i = game.getLevel().getItems().get(j);
@@ -39,6 +65,10 @@ public class ItemInteractionUpdater extends Updater<Player> {
         }
     }
 
+    /**
+     * Method that handles the player picking up an item
+     * @param i
+     */
     private void pickUpItem(Item i) {
         dropItem();
         getGameObject().pickUpItem(i);
