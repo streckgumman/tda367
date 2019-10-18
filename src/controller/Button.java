04cf08a13b1bbb57749c5644b252ca5da8dfe7bd
nilsanna <nilsson.j.anna@gmail.com>
2019-10-18 11:14:09 +0200

    Added documentation

--- src/controller/Button.java
+++ src/controller/Button.java
@@ -2,5 +2,8 @@ package controller;
 
 public abstract class Button {
 
+    /**
+     * The abstract public method that are implemented in subclasses
+     */
     public abstract void onClick();
 }
