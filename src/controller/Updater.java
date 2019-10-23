93ad86fa175d5027e226a1634e1c5053486eed76
nilsanna <nilsson.j.anna@gmail.com>
2019-10-23 14:55:11 +0200

    Added documentation

--- src/controller/Updater.java
+++ src/controller/Updater.java
@@ -4,6 +4,11 @@ import model.GameObject;
 
 import java.awt.event.KeyEvent;
 
+/**
+ * Abstract class that handles key input
+ *
+ * @param <T>
+ */
 public abstract class Updater<T extends GameObject> {
 
     private T gameObject;
@@ -12,10 +17,25 @@ public abstract class Updater<T extends GameObject> {
         this.gameObject = gameObject;
     }
 
+    /**
+     * Handles key input
+     *
+     * @param event
+     */
     public abstract void keyPressed(KeyEvent event);
 
+    /**
+     * Handles key input
+     *
+     * @param event
+     */
     public abstract void keyReleased(KeyEvent event);
 
+    /**
+     * Handles key input
+     *
+     * @param event
+     */
     public abstract void keyTyped(KeyEvent event);
 
     protected T getGameObject() {
